b0e8b175e678acd0fe0bf5478986b043a374e927
Oscar_Flores_Gonzalez <oflores3123@outlook.com>
2023-02-27 01:51:47 -0500

    Updated Day 1 Part 2 to use a min heap

--- Day 1/Part 2/top_x_highest_cal.py
+++ Day 1/Part 2/top_x_highest_cal.py
@@ -1,3 +1,5 @@
+import heapq
+
 with open ('Day 1/Part 1/input.txt') as input:
     data = input.readlines()
 
@@ -5,11 +7,12 @@ data.append('\n')
 #returns sum of calories carried by the top num(default 1) elves
 def get_top_num_calories(num=1):
     highest_totals = [0] * num
+    heapq.heapify(highest_totals)
     current_cal = 0
     for line in data:
         if line == '\n':
-            highest_totals.sort()
-            highest_totals[0] = current_cal if current_cal > highest_totals[0] else highest_totals[0]
+            if current_cal > highest_totals[0]:
+                heapq.heapreplace(highest_totals, current_cal)
             current_cal = 0
             continue
         current_cal += int(line)
