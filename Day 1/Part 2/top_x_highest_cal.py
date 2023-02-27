7abe7788c7c1c86d1c416acd82c9a65ce59be6f2
Oscar_Flores_Gonzalez <oflores3123@outlook.com>
2023-02-27 02:20:20 -0500

    Day 1 part 2 using heapq - cleaned up

--- Day 1/Part 2/top_x_highest_cal.py
+++ Day 1/Part 2/top_x_highest_cal.py
@@ -5,13 +5,15 @@ with open ('Day 1/Part 1/input.txt') as input:
 
 data.append('\n')
 #returns sum of calories carried by the top num(default 1) elves
+
 def get_top_num_calories(num=1):
-    highest_totals = [0] * num
-    heapq.heapify(highest_totals)
+    highest_totals = []
     current_cal = 0
     for line in data:
         if line == '\n':
-            if current_cal > highest_totals[0]:
+            if len(highest_totals) < num:
+                heapq.heappush(highest_totals, current_cal)
+            elif current_cal > highest_totals[0]:
                 heapq.heapreplace(highest_totals, current_cal)
             current_cal = 0
             continue
