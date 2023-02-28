35ad9a774eb0fb04e3e9b4ac16e3085c10b67a40
Oscar_Flores_Gonzalez <oflores3123@outlook.com>
2023-02-27 22:03:06 -0500

    comments

--- Day 3/part2.py
+++ Day 3/part2.py
@@ -1,6 +1,9 @@
 with open ('Day 3/input.txt') as input:
     data = input.readlines()
 
+'''
+Cool improvement to make: write a function that will take in N elves (3 in this problem's case but should be able to handle any)
+'''
 total = 0
 for i in range(0, len(data), 3):
     elf1, elf2, elf3 = data[i].strip(), data[i+1].strip(), data[i+2].strip()
