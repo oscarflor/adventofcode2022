79accd76134e3a96f245445e9f173315567e820d
Oscar_Flores_Gonzalez <oflores3123@outlook.com>
2023-02-27 22:28:37 -0500

    day 4 complete

---
+++ Day 4/part2.py
@@ -0,0 +1,13 @@
+with open ('Day 4/input.txt') as input:
+    data = input.readlines()
+
+total = 0
+for pair in data:
+    pairs = pair.strip().split(',')
+    pairs[0] = [int(i) for i in pairs[0].split('-')]
+    pairs[1] = [int(i) for i in pairs[1].split('-')]
+    pair1_set = set(range(pairs[0][0], pairs[0][1]+1))
+    pair2_set = set(range(pairs[1][0], pairs[1][1]+1))
+    if pair1_set & pair2_set:
+        total += 1
+print(total)
