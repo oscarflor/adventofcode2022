fd5e284ba99bf96f4239bd17443c52b1aa279692
Oscar_Flores_Gonzalez <oflores3123@outlook.com>
2023-02-27 21:42:59 -0500

    updated day3 p2 to use sets as well

--- Day 3/part2.py
+++ Day 3/part2.py
@@ -3,13 +3,12 @@ with open ('Day 3/input.txt') as input:
 
 total = 0
 for i in range(0, len(data), 3):
-    elf1, elf2, elf3 = data[i], data[i+1], data[i+2]
-    for item in elf1:
-        if item in elf2 and item in elf3:
-            if item.isupper():
-                total += ord(item) - 38
-            else:
-                total += ord(item) - 96
-            break
+    elf1, elf2, elf3 = data[i].strip(), data[i+1].strip(), data[i+2].strip()
+    intersection = set(elf1) & set(elf2) & set(elf3)
+    item = intersection.pop()
+    if item.isupper():
+        total += ord(item) - 38
+    else:
+        total += ord(item) - 96
 
 print(total)
